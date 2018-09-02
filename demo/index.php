9621c1b33baa2910bc488665cc0a13bb7fa65e14
Simon Anthony <github@torty.org.uk>
2018-09-03 08:52:12 +1000

    Weather working
    Geo location works
    weather works, but the two don't talk to eachother yet

--- demo/index.php
+++ demo/index.php
@@ -152,116 +152,22 @@ function getLocation() {
 }
 function showPosition(position) {
     console.log("Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude); 
+    " !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Longitude: " + position.coords.longitude); 
     x.innerHTML = "Latitude: " + position.coords.latitude + 
     "<br>Longitude: " + position.coords.longitude; 
-  lat = position.coords.latitude;
-  long = position.coords.longitude;
+ var lat = position.coords.latitude;
+var  lng = position.coords.longitude;
+  jsonCoordURL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&key=AIzaSyAco2DDdmDtvlETyITe9KvFGNDPOLFPEu4';
+      
+place_name = getJSON(jsonCoordURL)
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~place_name = "+ place_name);
+     
+  return lat,lng;
 }
            
              getLocation();
- function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: lat, lng: long}
-        });
-        var geocoder = new google.maps.Geocoder;
-        var infowindow = new google.maps.InfoWindow;
+           
 
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeLatLng(geocoder, map, infowindow);
-        });
-      }
-
-      function geocodeLatLng(geocoder, map, infowindow) {
-        var input = document.getElementById('latlng').value;
-        var latlngStr = input.split(',', 2);
-        var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
-        geocoder.geocode({'location': latlng}, function(results, status) {
-          if (status === 'OK') {
-            if (results[0]) {
-              map.setZoom(11);
-              var marker = new google.maps.Marker({
-                position: latlng,
-                map: map
-              });
-              infowindow.setContent(results[0].formatted_address);
-              infowindow.open(map, marker);
-            } else {
-              window.alert('No results found');
-            }
-          } else {
-            window.alert('Geocoder failed due to: ' + status);
-          }
-        });
-      }
-           var lat= -33.8859894;
-           var long = 151.2089373;
-</script>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <title>Reverse Geocoding</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #floating-panel {
-        position: absolute;
-        top: 10px;
-        left: 25%;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-        text-align: center;
-        font-family: 'Roboto','sans-serif';
-        line-height: 30px;
-        padding-left: 10px;
-      }
-      #floating-panel {
-        position: absolute;
-        top: 5px;
-        left: 50%;
-        margin-left: -180px;
-        width: 350px;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-      }
-      #latlng {
-        width: 225px;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="floating-panel">
-      <input id="latlng" type="text" value="lat,long">
-      <input id="submit" type="button" value="Reverse Geocode">
-    </div>
-    <div id="map"></div>
-    <script>
-     
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAxHVOox0XMNY0ykWjU065NPTeJeXdJZB4&callback=initMap">
-    </script>
-  </body>
-</html>
-   
-   
-    <script>
    function getJSON(url) {
         var resp ;
         var xmlHttp ;
@@ -301,7 +207,6 @@ geoLocation='Nottingham,uk';
       console.log("!!!!!!!!!!!!"+tempC);
 
 
-     
      </script>
     
       <?php echo($data['weather'][0][description]) ?>
